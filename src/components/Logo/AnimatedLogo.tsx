ed8b641ff9fbb0d1e36dcfda2c3020f046ffa7f8
SHENID <dineshgembler@gmail.com>
2024-06-17 02:21:52 +0300

    first commit

---
+++ src/components/Logo/AnimatedLogo.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import cl from "./style.module.css"
+
+const AnimatedLogo = () =>{
+
+
+    return (
+        <>
+        <div className={cl.backimg}>
+            <div className={cl.cont}>
+                <div className={cl.backlogo}>
+                    SHENID
+                </div>
+            </div>
+        </div>
+    </>
+    );
+};
+
+export default AnimatedLogo;
